649e41ebc2e3f82c2d5289446164a0db1623610d
v.yusupov <vladusupov2810@gmail.com>
2023-11-29 12:30:22 +0200

    (feat): Created and migrated Goal entity.

--- QuickList.Infrastructure/DataAccess/QuickListContext.cs
+++ QuickList.Infrastructure/DataAccess/QuickListContext.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using QuickList.Domain.GoalAggregate;
 
 namespace QuickList.Infrastructure.DataAccess;
 
 public class QuickListContext : DbContext
 {
+    public DbSet<Goal> Goals { get; set; }
+    
     public QuickListContext(DbContextOptions<QuickListContext> options) : base(options) { }
 }
